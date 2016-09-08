6367332ecc7eb1539fcfa78fbcde5ca33b256aa5
Divye Gupta <divyegupta@Divyes-MacBook-Air-2.local>
2016-09-08 10:13:16 -0400

    Displaying the fetched results

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -12,23 +12,76 @@ import {connect} from 'react-redux'
 
 class Home extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state={ingredientsInput: ''}
+  }
   searchPressed(){
-    this.props.fetchRecipes('chicken,potato')
+    this.setState({searching: true});
+    this.props.fetchRecipes(this.state.ingredientsInput).then( () => {
+      this.setState({searching: false})
+    })
+  }
+
+  recipes(){
+    return Object.keys(this.props.searchedRecipes).map(key => this.props.searchedRecipes[key])
   }
 
   render(){
-    return <View style= {{marginTop:20}}>
-      <View>
-        <TouchableHighlight onPress={ () => this.searchPressed()}>
+    return <View style= {styles.container}>
+      <View style = {styles.searchSection}>
+        <TextInput style={styles.searchInput}
+          returnKeyType='search'
+          placeholder='Ingredients - Separated by commas'
+          onChangeText={(ingredientsInput) => this.setState({ingredientsInput})}
+          value={this.state.ingredientsInput}
+        />
+        <TouchableHighlight onPress={ () => this.searchPressed()} style={styles.searchButton}>
           <Text>Search Recipes</Text>
         </TouchableHighlight>
       </View>
-      <ScrollView>
+      <ScrollView style={styles.scrollSection}>
+        {!this.state.searching && this.recipes().map((recipe) => {
+          return <View key={recipe.id}>
+          <Image source={{uri:recipe.image}} style={styles.fetchedImage} />
+          <Text style={styles.fetchedText}>{recipe.title}</Text>
+          </View>
+        })}
+          {this.state.searching ? <Text>Searching...</Text> : null}
       </ScrollView>
     </View>
   }
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  searchSection: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5,
+    flex: 1
+  },
+  scrollSection: {
+    flex: 7
+  },
+  fetchedImage: {
+    height: 150,
+  },
+  fetchedText: {
+    backgroundColor: 'black',
+    color: 'white',
+    height: 20,
+  },
+  searchInput: {
+    flex: 7
+  },
+  searchButton: {
+    flex: 2
+  }
+})
 
 function mapStateToProps(state) {
   return {
--- app/reducers/recipes.js
+++ app/reducers/recipes.js
@@ -1,11 +1,20 @@
 import createReducer from '../lib/createReducer'
 import * as types from '../actions/types'
 
-export const searchedRecipes = createReducer({},{});
+export const searchedRecipes = createReducer({},{
+  [types.SET_SEARCHED_RECIPES](state, action) {
+    let newState = {};
+    //grab the array and store it as a hashmap
+    action.recipes.forEach((recipe) => {
+      newState[recipe.id] = recipe
+    });
+    return newState;
+  }
+});
 
 //giving the initial values and then increasing the value on each action
 export const recipeCount = createReducer(0, {
-  [types.ADD_RECIPE](state,action){
-    return state + 1
+  [types.SET_SEARCHED_RECIPES](state,action){
+    return action.recipes.length
   }
 });
